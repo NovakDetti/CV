c06bf2ed43419990dbdd1ea92e39421a34c814b9
NovakDetti <54432325+NovakDetti@users.noreply.github.com>
2020-06-08 13:24:16 +0200

    Updates

---
+++ precache-manifest.3756081817e866acfd09707b02924c54.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1f91d0f24eb4ab91717342152d7007",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "81fe4df08a81986fd282",
+    "url": "/CV/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "edc2bc77367697cdc664",
+    "url": "/CV/static/css/main.d84f3e62.chunk.css"
+  },
+  {
+    "revision": "81fe4df08a81986fd282",
+    "url": "/CV/static/js/2.af9c0889.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/CV/static/js/2.af9c0889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc2bc77367697cdc664",
+    "url": "/CV/static/js/main.b1dca864.chunk.js"
+  },
+  {
+    "revision": "f9b12c8678ae263415d0",
+    "url": "/CV/static/js/runtime-main.57628258.js"
+  },
+  {
+    "revision": "c02fc916a750f8b7d0636c1418f3e92c",
+    "url": "/CV/static/media/bubbly.c02fc916.png"
+  },
+  {
+    "revision": "6d815877154c67d71f5f76d4d643e151",
+    "url": "/CV/static/media/creatify.6d815877.png"
+  },
+  {
+    "revision": "06b3c508afb1c86ee0a40b17d35541f4",
+    "url": "/CV/static/media/cv.06b3c508.png"
+  },
+  {
+    "revision": "59ce4711d32c52897d36a2b1c7497f13",
+    "url": "/CV/static/media/degree.59ce4711.jpg"
+  },
+  {
+    "revision": "aa896e171dbe350d1a4347bfb581c1eb",
+    "url": "/CV/static/media/docvis.aa896e17.png"
+  },
+  {
+    "revision": "7ffc28613b61d0f5cf03e448fbb19f27",
+    "url": "/CV/static/media/elte.7ffc2861.jpeg"
+  },
+  {
+    "revision": "7779ae9a7c1588747631a444c75945f8",
+    "url": "/CV/static/media/landing.7779ae9a.png"
+  },
+  {
+    "revision": "41ca00f0af6009a8acfef7ceb7430b8a",
+    "url": "/CV/static/media/macbook.41ca00f0.JPG"
+  },
+  {
+    "revision": "f0c21d3f7e51293c45edd1490e8fb053",
+    "url": "/CV/static/media/mail.f0c21d3f.png"
+  },
+  {
+    "revision": "e87a9f98d84be38bd65c5326ef34fab3",
+    "url": "/CV/static/media/me.e87a9f98.png"
+  },
+  {
+    "revision": "cce610ee46300726cf08ba49e2ae30d7",
+    "url": "/CV/static/media/starry-sky-998641.cce610ee.jpg"
+  },
+  {
+    "revision": "1465038c6f7dd7aad2302ba67c48db58",
+    "url": "/CV/static/media/work.1465038c.jpeg"
+  }
+]);
